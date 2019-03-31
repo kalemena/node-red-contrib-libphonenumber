91662696d1e6c7419f4c99e63fe3963e61b0f438
kalemena <kalemena@free.fr>
2019-03-31 22:02:56 +0200

    Add sample from libphonenumber official site

--- phonenumber.js
+++ phonenumber.js
@@ -1,9 +1,34 @@
+const PNF = require('google-libphonenumber').PhoneNumberFormat;
+const phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
+ 
 module.exports = function(RED) {
     function PhoneNumberNode(config) {
         RED.nodes.createNode(this,config);
         var node = this;
         this.on('input', function(msg) {
-            msg.payload = msg.payload.toLowerCase();
+            
+            // Parse number with country code and keep raw input.
+            const number = phoneUtil.parseAndKeepRawInput(msg.payload, 'US');
+            
+            msg.phone = {};
+            msg.phone.CountryCode = number.getCountryCode();
+            msg.phone.NationalNumber = number.getNationalNumber();
+            msg.phone.Extension = number.getExtension();
+            msg.phone.CountryCodeSource = number.getCountryCodeSource();
+            msg.phone.ItalianLeadingZero = number.getItalianLeadingZero();
+            msg.phone.RawInput = number.getRawInput();
+            msg.phone.isPossibleNumber = phoneUtil.isPossibleNumber(number);
+            msg.phone.isValidNumber = phoneUtil.isValidNumber(number);
+            msg.phone.isValidNumberForRegion = phoneUtil.isValidNumberForRegion(number, 'US');
+            msg.phone.RegionCodeForNumber = phoneUtil.getRegionCodeForNumber(number);
+            msg.phone.NumberType = phoneUtil.getNumberType(number);
+            msg.phone.E164 = phoneUtil.format(number, PNF.E164);
+            msg.phone.OriginalFormat = phoneUtil.formatInOriginalFormat(number, 'US');
+            msg.phone.NationalFormat = phoneUtil.format(number, PNF.NATIONAL);
+            msg.phone.InternationalFormat = phoneUtil.format(number, PNF.INTERNATIONAL);
+            msg.phone.OutOfCountryCallingNumberUS = phoneUtil.formatOutOfCountryCallingNumber(number, 'US');
+            msg.phone.OutOfCountryCallingNumberCH = phoneUtil.formatOutOfCountryCallingNumber(number, 'CH');
+
             node.send(msg);
         });
     }
